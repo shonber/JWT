2c7d431f9c9e0f146a9cf4b55c9a7fe6655358b4
ShonBer <32219039+shonber@users.noreply.github.com>
2022-04-25 08:06:40 +0300

    Add files via upload

---
+++ JWT/maIN.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'BeforeValidException.php';
+require_once 'ExpiredException.php';
+require_once 'SignatureInvalidException.php';
+require_once 'JWT.php';
+use \Firebase\JWT\JWT;
+
+$public_key = file_get_contents('keys/private.pem');
+
+echo JWT::encode(array(
+	# Issuer
+	"iss" => "https://www.itsafe.co.il",
+	
+	# Issued at
+	"iat" => time(),
+	
+	# Expire
+	"exp" => time() + 120,
+	
+	"data" => [
+		"name" => "admin"
+	]
+), $public_key, 'RS256');
